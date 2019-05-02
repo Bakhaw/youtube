05827626cb4b8856b4dd0f041e235399fac86b14
Faïssal <faissal@MacBook-Pro-de-Faissal.local>
2019-05-02 16:09:12 +0200

    :construction_worker: on useFetch

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -4,14 +4,14 @@ import API from '../api';
 function useFetch(methodName, methodParams = [], initialState = []) {
   const [data, setData] = useState(initialState);
 
-  useEffect(() => {
-    async function fetchData() {
-      const req = await API[methodName](...methodParams);
-      await setData(req);
-    }
+  async function fetchData() {
+    const req = await API[methodName](...methodParams);
+    await setData(req);
+  }
 
+  useEffect(() => {
     fetchData();
-  }, [methodName, ...methodParams]);
+  }, [...methodParams]);
 
   return data;
 }
