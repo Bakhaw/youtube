237e19567d628d30864d414a73997d2bd65472db
Faïssal <faissal@MacBook-Pro-de-Faissal.local>
2019-05-14 17:06:43 +0200

    use <Comments /> in <Video /> screen

--- src/screens/Video/index.js
+++ src/screens/Video/index.js
@@ -1,18 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
 
+import Comments from '../../components/Comments';
 import Container from '../../components/Container';
 import VideoPlayer from '../../components/VideoPlayer';
+import { dummyVideo, dummyVideoComments } from '../../api/dummy-data';
 import { useFetch } from '../../hooks';
 
-function Video(props) {
-  const { videoId } = props.match.params;
-  const video = useFetch('getVideoById', [videoId]);
+const LeftContentWrapper = styled.div`
+  width: 62vw;
+`;
 
-  if (video.length === 0) return <p>Loading!</p>;
+const RightContentWrapper = styled.div`
+  min-height: calc(100vh - 70px); /* 70px is <NavBar /> height */
+  width: 38vw;
+  border: 2px solid palevioletred;
+`;
+
+function Video({ match }) {
+  const { videoId } = match.params;
+  // const videoDesc = useFetch('getVideoById', [videoId]);
+  // const videoComments = useFetch('getVideoComments', [videoId]);
+
+  if (dummyVideo.length === 0) return <p>Loading!</p>;
 
   return (
     <Container>
-      <VideoPlayer item={video[0]} />
+      <LeftContentWrapper>
+        <VideoPlayer item={dummyVideo[0]} />
+        <Comments comments={dummyVideoComments} />
+      </LeftContentWrapper>
+      <RightContentWrapper />
     </Container>
   );
 }
