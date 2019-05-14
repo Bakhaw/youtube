e460ac0aa661bf2c3d77ccffc75c0db0b888e985
Faïssal <faissal@MacBook-Pro-de-Faissal.local>
2019-05-14 17:06:27 +0200

    :art: on <Container />

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
+  display: flex;
   padding: 20px;
   margin-top: 50px;
-  min-height: calc(100vh - 70px);
+  min-height: calc(100vh - 70px); /* 70px is <NavBar /> height */
   max-width: 100vw;
   background: #ffeff6;
   @media (max-width: 800px) {
