89a87cd2182643de5ff0ee0432e429b0ba98851b
Faïssal <faissal@MacBook-Pro-de-Faissal.local>
2019-05-14 11:02:02 +0200

    :art: on <NavBar />

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -18,8 +18,7 @@ const Wrapper = styled.header`
   height: 60px;
   padding: 0 20px;
   background: #fff;
-  box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
-    0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
+  box-shadow: 0 10px 14px 0 rgba(0, 0, 0, 0.06);
   .Drawer {
     grid-column: 1;
   }
