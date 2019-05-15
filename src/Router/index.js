a272714dfb0b7e3a0908f0d0dc3a8bbec1f2dab8
Faïssal <faissal@MacBook-Pro-de-Faissal.local>
2019-05-15 14:20:07 +0200

    add lazy load and suspense

--- src/Router/index.js
+++ src/Router/index.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import Loader from '../components/Loader';
 import NavBar from '../components/NavBar';
-import Home from '../screens/Home';
-import SearchResults from '../screens/SearchResults';
-import Trending from '../screens/Trending';
-import Video from '../screens/Video';
+
+const Home = lazy(() => import('../screens/Home'));
+const SearchResults = lazy(() => import('../screens/SearchResults'));
+const Trending = lazy(() => import('../screens/Trending'));
+const Video = lazy(() => import('../screens/Video'));
+
+// import NavBar from '../components/NavBar';
+// import Home from '../screens/Home';
+// import SearchResults from '../screens/SearchResults';
+// import Trending from '../screens/Trending';
+// import Video from '../screens/Video';
 
 function Router() {
   return (
     <BrowserRouter>
       <>
         <NavBar />
-        <Switch>
-          <Route path='/search/:searchQuery' component={SearchResults} />
-          <Route path='/watch/:videoId' component={Video} />
-          <Route path='/trending' component={Trending} />
-          <Route path='/' component={Home} />
-        </Switch>
+        <Suspense fallback={<Loader />}>
+          <Switch>
+            <Route path='/search/:searchQuery' component={SearchResults} />
+            <Route path='/watch/:videoId' component={Video} />
+            <Route path='/trending' component={Trending} />
+            <Route path='/' component={Home} />
+          </Switch>
+        </Suspense>
       </>
     </BrowserRouter>
   );
