520f17c0f67c694030bc003d226b81f6e570bf67
Faïssal <faissal@MacBook-Pro-de-Faissal.local>
2019-05-15 12:13:15 +0200

    add getRelatedVideos in api/

--- src/api/index.js
+++ src/api/index.js
@@ -3,12 +3,25 @@ import config from './config';
 
 const API = {
   getTrendingVideos: async function() {
+    // ? maxResults 1 to 50
     try {
-      const url = `https://www.googleapis.com/youtube/v3/videos?part=snippet%2Cstatistics&chart=mostPopular&regionCode=FR&maxResults=5&key=${
+      const url = `https://www.googleapis.com/youtube/v3/videos?part=snippet%2Cstatistics&chart=mostPopular&regionCode=FR&maxResults=50&key=${
         config.apiKey
       }`;
       const { data } = await axios.get(url);
       return data;
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  getRelatedVideos: async function(videoId) {
+    // ? maxResults 1 to 50
+    try {
+      const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=${videoId}&type=video&maxResults=50&key=${
+        config.apiKey
+      }`;
+      const { data } = await axios.get(url);
+      return data.items;
     } catch (err) {
       console.log(err);
     }
@@ -36,8 +49,9 @@ const API = {
     }
   },
   getVideoComments: async function(videoId) {
+    // ? maxResults 1 to 100
     try {
-      const url = `https://www.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&order=relevance&videoId=${videoId}&key=${
+      const url = `https://www.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&order=relevance&videoId=${videoId}&maxResults=100&key=${
         config.apiKey
       }`;
       const { data } = await axios.get(url);
@@ -47,8 +61,9 @@ const API = {
     }
   },
   searchByQuery: async function(query) {
+    // ? maxResults 1 to 50
     try {
-      const url = ` https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${query}&key=${
+      const url = ` https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=${query}&key=${
         config.apiKey
       }`;
       const { data } = await axios.get(url);
